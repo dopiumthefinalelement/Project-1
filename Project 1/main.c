7bef68f92b5bc4523b13eb882726da02c05e8b99
skywalker_OG <hotsaucejalapeno@gmail.com>
2016-08-29 11:00:55 -0500

    Initial Commit

---
+++ Project 1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project 1
+//
+//  Created by skywalker_OG on 8/29/16.
+//  Copyright © 2016 Michael Long. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
